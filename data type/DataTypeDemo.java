cbbc722f26b605002ca4196bfbc776e41457d20f
ashirwadkhairnar12 <119682012+ashirwadkhairnar12@users.noreply.github.com>
2022-12-03 12:20:51 +0530

    Add files via upload

---
+++ data type/DataTypeDemo.java
@@ -0,0 +1,41 @@
+package basic;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		boolean IoT = false;
+		 System.out.println(IoT);
+
+		 byte range;
+		 range = 127;
+		 System.out.println(range);
+		 
+		 short range1=-127;
+		 System.out.println(range1);
+		 
+		 int range2=300000000;
+		 System.out.println(range2);
+		 
+		 long range3=30000000000L;
+		 System.out.println(range2);
+		
+		 double number = 99.9;
+		 System.out.println(number);
+
+	
+		 float number1 = 99.9f;
+		 System.out.println(number1);
+
+		
+		 char letter = '\u0041';
+		 System.out.println(letter);
+		 
+		 char letter2 = 65;
+		 System.out.println(letter2);
+		 
+		 char letter3 = 'A';
+		 System.out.println(letter3);
+
+	}
+
+}
